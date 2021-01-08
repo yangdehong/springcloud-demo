12a36f67803533fb91854164c2c5c19d0686cfd7
yangdehong <405104787@qq.com>
2021-01-08 10:10:07 +0800

    hystrix-dashboard

---
+++ hystrix-dashboard/src/main/java/com/ydh/redsheep/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,21 @@
+package com.ydh.redsheep.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 访问地址 http://localhost:9000/hystrix
+ * 兼容地址 http://localhost:8090/actuator/hystrix.stream
+ */
+@EnableDiscoveryClient
+@EnableHystrixDashboard  // 开启hystrix dashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
--- service-autodeliver/src/main/java/com/ydh/redsheep/serviceautodeliver/ServiceAutodeliverApplication.java
+++ service-autodeliver/src/main/java/com/ydh/redsheep/serviceautodeliver/ServiceAutodeliverApplication.java
@@ -1,5 +1,6 @@
 package com.ydh.redsheep.serviceautodeliver;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
@@ -28,18 +29,18 @@ public class ServiceAutodeliverApplication {
 
 
     /**
-     * 在被监控的微服务中注册一个serlvet，后期我们就是通过访问这个servlet来获取该服务的Hystrix监控数据的
+     * 在被监控的微服务中注册一个serlvet，后期我们就是通过访问这个servlet来获取该服务的Hystrix监控数据(hystrix-dashboard)的
      * 前提：被监控的微服务需要引入springboot的actuator功能
      * @return
      */
-//    @Bean
-//    public ServletRegistrationBean getServlet(){
-//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-//        registrationBean.setLoadOnStartup(1);
-//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-//        registrationBean.setName("HystrixMetricsStreamServlet");
-//        return registrationBean;
-//    }
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }

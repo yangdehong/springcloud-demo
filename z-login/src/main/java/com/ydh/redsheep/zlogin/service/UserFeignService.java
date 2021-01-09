d68536dfc88c3b4dad2c2842ae948c72850c5c26
yangdehong <405104787@qq.com>
2021-01-09 23:53:02 +0800

    login

--- z-login/src/main/java/com/ydh/redsheep/zlogin/service/UserFeignService.java
+++ z-login/src/main/java/com/ydh/redsheep/zlogin/service/UserFeignService.java
@@ -1,5 +1,6 @@
 package com.ydh.redsheep.zlogin.service;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 

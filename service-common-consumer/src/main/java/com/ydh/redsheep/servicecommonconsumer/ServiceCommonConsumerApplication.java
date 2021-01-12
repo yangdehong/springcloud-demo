014b8550193c3cd00cd2767cc14a0c53890a9658
yangdehong <405104787@qq.com>
2021-01-12 12:45:44 +0800

    sleuth

---
+++ service-common-consumer/src/main/java/com/ydh/redsheep/servicecommonconsumer/ServiceCommonConsumerApplication.java
@@ -0,0 +1,13 @@
+//package com.ydh.redsheep.servicecommonconsumer;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ServiceCommonConsumerApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ServiceCommonConsumerApplication.class, args);
+//    }
+//
+//}

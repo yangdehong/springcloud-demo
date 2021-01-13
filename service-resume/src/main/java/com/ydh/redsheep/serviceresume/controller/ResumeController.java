6dea0911817bb13c4eae167df0599b7561cdb2a2
yangdehong <405104787@qq.com>
2021-01-13 11:48:21 +0800

    scn

--- service-resume/src/main/java/com/ydh/redsheep/serviceresume/controller/ResumeController.java
+++ service-resume/src/main/java/com/ydh/redsheep/serviceresume/controller/ResumeController.java
@@ -2,7 +2,6 @@ package com.ydh.redsheep.serviceresume.controller;
 
 import com.ydh.redsheep.serviceresume.service.ResumeService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
